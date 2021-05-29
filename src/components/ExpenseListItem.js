475dbf06d3fbff5444f071c88f0e3edd8a13308f
Saul Ansbacher <sansbacher@gmail.com>
2021-05-28 22:19:13 -0600

    Setup formmating on ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'			// Like moment, but for numbers
 
 // The ExpenseListItem is called with {...expense} which JSX spreads properties to individual properties
 // They all come through as props, as usual. Which we destructure from props.
@@ -8,7 +10,11 @@ const ExpenseListItem = ({description, amount, createdAt, id}) => (
 		<Link to={`/edit/${id}`}>
 			<h3>{description}</h3>
 		</Link>
-		<p>{amount} - {createdAt}</p>
+		<p>
+			{numeral(amount / 100).format('$0,0.00')}
+			- 
+			{moment(createdAt).format('MMMM Do, YYYY')}
+		</p>
 	</div>
 )
 
