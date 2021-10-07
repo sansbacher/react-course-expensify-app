eb09a733d7f9fd027deff61b7815b2d5742f70d6
Saul Ansbacher <sansbacher@gmail.com>
2021-10-06 21:51:19 -0600

    Added show hidden or filtered expenses on ExpensesSummary dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,7 +6,9 @@ import selectExpenses from '../selectors/expenses'
 import selectExpenseTotal from '../selectors/expenses-total'
 
 // Named export for testing purposes with Jest
-export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
+export const ExpensesSummary = ({allExpensesCount, expensesCount, expensesTotal}) => {
+	const hiddenExpensesCount = allExpensesCount - expensesCount
+	const hiddenExpensesWord = hiddenExpensesCount === 1 ? 'expense' : 'expenses'
 	const expenseWord = expensesCount === 1 ? 'expense' : 'expenses'
 	const formattedTotal = numeral(expensesTotal / 100).format('$0,0.00')
 
@@ -14,6 +16,7 @@ export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
 		<div className="page-header">
 			<div className="content-container">
 				<h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{formattedTotal}</span></h1>
+				{hiddenExpensesCount !== 0 && <p className="page-header__message">With {hiddenExpensesCount} {hiddenExpensesWord} not shown due to current filters</p>}
 				<div className="page-header__actions">
 					<Link className="button" to="/create">Add Expense</Link>
 				</div>
@@ -23,9 +26,11 @@ export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
 }
 
 const mapStateToProps = (state) => {
+	const allExpensesCount = state.expenses.length
 	const visibleExpenses = selectExpenses(state.expenses, state.filters)
 	
 	return {
+		allExpensesCount,
 		expensesCount: visibleExpenses.length,
 		expensesTotal: selectExpenseTotal(visibleExpenses)
 	};
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -5,13 +5,24 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import {ExpensesSummary} from '../../components/ExpensesSummary'
 
-test('should render ExpensesSummary with 1 expense', () => {
+test('should render ExpensesSummary with 1 expense, all', () => {
 	// Just need to render the Component, the selectExpenses() function is tested under selectors
-	const wrapper = shallow(<ExpensesSummary expensesCount={1} expensesTotal={12300}/>)
+	const wrapper = shallow(<ExpensesSummary expensesCount={1} expensesTotal={12300} allExpensesCount={1} />)
 	expect(wrapper).toMatchSnapshot()
 })
 
-test('should render ExpensesSummary with multiple expenses', () => {
-	const wrapper = shallow(<ExpensesSummary expensesCount={2} expensesTotal={45600} />)
+test('should render ExpensesSummary with multiple expenses, all', () => {
+	const wrapper = shallow(<ExpensesSummary expensesCount={2} expensesTotal={45600} allExpensesCount={2} />)
+	expect(wrapper).toMatchSnapshot()
+})
+
+test('should render ExpensesSummary with 1 expense, filtered', () => {
+	// Just need to render the Component, the selectExpenses() function is tested under selectors
+	const wrapper = shallow(<ExpensesSummary expensesCount={1} expensesTotal={65400} allExpensesCount={5} />)
+	expect(wrapper).toMatchSnapshot()
+})
+
+test('should render ExpensesSummary with multiple expenses, filtered', () => {
+	const wrapper = shallow(<ExpensesSummary expensesCount={2} expensesTotal={78900} allExpensesCount={3} />)
 	expect(wrapper).toMatchSnapshot()
 })
